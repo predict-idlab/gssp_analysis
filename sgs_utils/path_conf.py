e58ddcf6c20b2171a85a1f4c179d954dfa4dc7e1
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2022-02-09 17:24:53 +0100

    :bar_chart: adding notebooks

---
+++ sgs_utils/path_conf.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# speech_data_root_dir = Path('/speechdump/jonvdrdo/data/speech_web_app')
+# speech_data_root_dir = Path('/media/speech_webapp')
+speech_data_root_dir = Path('/users/jonvdrdo/jonas/data/speech_webapp')
+
+speech_data_session_dir = speech_data_root_dir.joinpath('backup')
+
+speech_web_app_image_dir = speech_data_root_dir.joinpath('img')
+
+loc_data_dir = Path('../loc_data/')
