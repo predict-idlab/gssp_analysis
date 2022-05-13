6d1d6e4525cb8d1f736872781bd0f2e3afd068c1
mitchelkappen <36090493+mitchelkappen@users.noreply.github.com>
2022-05-13 10:39:39 +0200

    Update 1.0_FactorAnalysis.R
    Generate CFA en cronbach's alpha for both arousal and valence score.
    Also visual representation of CFA results generated using lavaanPlot

--- scripts/1.0_FactorAnalysis.R
+++ scripts/1.0_FactorAnalysis.R
@@ -6,6 +6,8 @@ options(contrasts=c("contr.sum", "contr.poly")) # Set contrast settings to effec
 
 # Libraries
 library(arrow)
+library(lavaan)
+library(lavaanPlot)
 
 # Set and Get directories
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #Set WD to script location
@@ -16,32 +18,55 @@ piscesData <- imageData[imageData$DB == 'PiSCES',]
 radboudData <- imageData[imageData$DB == 'Radboud',]
 marloesData <- imageData[imageData$DB == 'Marloes',]
 
-# Cronbach's Alpha
 library(ltm)
-# Arousal
-piscesDataClean = piscesData[c("ID", "pic_name","arousal")]
-piscesDataClean$pic_name = as.factor(piscesDataClean$pic_name)
-piscesDataClean = reshape(piscesDataClean, idvar = "ID", timevar = "pic_name", direction = "wide") # Drops two answers, two people answered 1 trial twice?
-cronbach.alpha(piscesDataClean, CI=TRUE, na.rm = TRUE)
-
-# Valence
+##### Valence #####
 piscesDataClean = piscesData[c("ID", "pic_name","valence")]
 piscesDataClean$pic_name = as.factor(piscesDataClean$pic_name)
 piscesDataClean = reshape(piscesDataClean, idvar = "ID", timevar = "pic_name", direction = "wide") # Drops two answers, two people answered 1 trial twice?
+
+# Cronbach's Alpha
 cronbach.alpha(piscesDataClean, CI=TRUE, na.rm = TRUE)
 
+# CFA
+names(piscesDataClean)[2:16] = c("Picture_105", "Picture_82",  "Picture_118", "Picture_65", "Picture_88", "Picture_87", "Picture_59", "Picture_93", "Picture_56", "Picture_81",
+                                 "Picture_110", "Picture_96",  "Picture_132", "Picture_80",  "Picture_98" )
 
+HS.model <- 'pisces =~ Picture_105 + Picture_82 + Picture_118 + Picture_65 + Picture_88 + Picture_87 + Picture_59 + Picture_93 + Picture_56 + Picture_81 + Picture_110 + Picture_96 + Picture_132 + Picture_80 + Picture_98'
 
-# PCA
-library(psych)
-library(lavaan)
-# fa.parallel(piscesData$arousal[,-1], fa="PC", ntrials=100,
-#             show.legend=FALSE, main="Scree plot with parallel analysis")
+fit <- cfa(HS.model, data=piscesDataClean, std.lv=TRUE)
+summary(fit, fit.measures=TRUE, standardized=TRUE)
 
-piscesDataClean = piscesData[c("ID", "pic_name","valence","arousal")]
-HS.model <- ' pisces  =~ valence + arousal'
+# Visualize CFA
+labels <- list(Picture_105 = "P_105", Picture_82 = "P_82", Picture_118 = "P_118", Picture_65 = "P_65", Picture_88 = "P_88", Picture_87 = "P_87", Picture_59 = "P_59", Picture_93 = "P_93", Picture_56 = "P_56", Picture_81 = "P_81", Picture_110 = "P_110", Picture_96 = "P_96", Picture_132 = "P32", Picture_80 = "P_80", Picture_98 = "P_98")
 
-fit <- cfa(HS.model, data=piscesDataClean)
-summary(fit, fit.measures=TRUE)
+# Source: https://cran.r-project.org/web/packages/lavaanPlot/vignettes/Intro_to_lavaanPlot.html
+# Basic
+lavaanPlot(model = fit, labels = labels, node_options = list(shape = "box", fontname = "Helvetica"), edge_options = list(color = "grey"), coefs = TRUE, sig = .05)
+# Standardized and with significance levels
+lavaanPlot(model = fit, labels = labels, node_options = list(shape = "box", fontname = "Helvetica"), edge_options = list(color = "grey"), coefs = TRUE, stand = TRUE, stars = "latent")
 
-# Conclusion: CFA doesn't work with 2 items
+##### Arousal #####
+piscesDataClean = piscesData[c("ID", "pic_name","arousal")]
+piscesDataClean$pic_name = as.factor(piscesDataClean$pic_name)
+piscesDataClean = reshape(piscesDataClean, idvar = "ID", timevar = "pic_name", direction = "wide") # Drops two answers, two people answered 1 trial twice?
+
+# Cronbach's Alpha
+cronbach.alpha(piscesDataClean, CI=TRUE, na.rm = TRUE)
+
+# CFA
+names(piscesDataClean)[2:16] = c("Picture_105", "Picture_82",  "Picture_118", "Picture_65", "Picture_88", "Picture_87", "Picture_59", "Picture_93", "Picture_56", "Picture_81",
+                                 "Picture_110", "Picture_96",  "Picture_132", "Picture_80",  "Picture_98" )
+
+HS.model <- 'pisces =~ Picture_105 + Picture_82 + Picture_118 + Picture_65 + Picture_88 + Picture_87 + Picture_59 + Picture_93 + Picture_56 + Picture_81 + Picture_110 + Picture_96 + Picture_132 + Picture_80 + Picture_98'
+
+fit <- cfa(HS.model, data=piscesDataClean, std.lv=TRUE)
+summary(fit, fit.measures=TRUE, standardized=TRUE)
+
+# Visualize CFA
+labels <- list(Picture_105 = "P_105", Picture_82 = "P_82", Picture_118 = "P_118", Picture_65 = "P_65", Picture_88 = "P_88", Picture_87 = "P_87", Picture_59 = "P_59", Picture_93 = "P_93", Picture_56 = "P_56", Picture_81 = "P_81", Picture_110 = "P_110", Picture_96 = "P_96", Picture_132 = "P32", Picture_80 = "P_80", Picture_98 = "P_98")
+
+# Source: https://cran.r-project.org/web/packages/lavaanPlot/vignettes/Intro_to_lavaanPlot.html
+# Basic
+lavaanPlot(model = fit, labels = labels, node_options = list(shape = "box", fontname = "Helvetica"), edge_options = list(color = "grey"), coefs = TRUE, sig = .05)
+# Standardized and with significance levels
+lavaanPlot(model = fit, labels = labels, node_options = list(shape = "box", fontname = "Helvetica"), edge_options = list(color = "grey"), coefs = TRUE, stand = TRUE, stars = "latent")
