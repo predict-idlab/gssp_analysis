279c737cc800eed97caec6c36b763a822e4f9cd7
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2022-06-23 17:42:30 +0200

    :dash:

--- sgs_utils/path_conf.py
+++ sgs_utils/path_conf.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 # speech_data_root_dir = Path('/speechdump/jonvdrdo/data/speech_web_app')
 # speech_data_root_dir = Path('/media/speech_webapp')
-speech_data_root_dir = Path('/users/jonvdrdo/jonas/data/speech_webapp')
+# speech_data_root_dir = Path('/users/jonvdrdo/jonas/data/speech_webapp')
+speech_data_root_dir = Path('/project_scratch/data/speech_web_app')
 
 speech_data_session_dir = speech_data_root_dir.joinpath('backup')
 
