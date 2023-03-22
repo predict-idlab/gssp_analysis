c3c2563ec30f5a45e3088d70c758084b3ac3f22c
jonas <jonvdrdo.vanderdonckt@ugent.be>
2023-03-22 22:11:04 +0100

    :sparkles: update notebooks

--- GSSP_utils/path_conf.py
+++ GSSP_utils/path_conf.py
@@ -5,13 +5,23 @@ from socket import gethostname
 
 if gethostname() == "gecko":
     data_dir = Path("/media/SPS/")  # this directory needs to be adjusted
-    speech_data_root_dir = data_dir / "speech_webapp_cleaned"
+    speech_data_root_dir = data_dir / "web_app_data"
 
     # Path towards the "Corpus Gesproken Nederlands" data
     cgn_root_dir = Path("/media/SPS/cgn/")
     cgn_ort_path = cgn_root_dir / "ort"
 else:
-    raise ValueError("Unknown hostname")
+    raise ValueError(
+        "Unknow user, please comment this line and add your own path below"
+    )
+    # the path to the osf `speech_web_app` data directory - https://osf.io/8wrmn
+    data_dir = Path("TODO")
+    speech_data_root_dir = data_dir / "web_app_data"
+
+    # If you want to use the CGN data, you need to download it yourself
+    # and add the path to the directory here
+    cgn_root_dir = Path("TODO")
+    cgn_ort_path = cgn_root_dir / "ort"
 
 speech_data_session_dir = speech_data_root_dir.joinpath("backup")
 speech_web_app_image_dir = speech_data_root_dir.joinpath("img")
@@ -26,4 +36,5 @@ loc_data_dir = Path(getsourcefile(lambda: 0)).parent.parent.absolute() / "loc_da
 
 
 # The date on which users were recuited via the prolific platform
+# Might be useful for future analysis
 prolific_date = datetime(2022, 6, 22)
